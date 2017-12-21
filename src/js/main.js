bdc20cfee0904063c23d3e25bebc350710f446b7
alexey.dovgal <alexey.dovgal@effective-soft.com>
2017-12-21 17:48:13 +0300

    desktop version

--- src/js/main.js
+++ src/js/main.js
@@ -4,5 +4,17 @@ $(".nav-link").click(function(e) {
     $(".nav-item").removeClass("active");
     activeLink.addClass("active");
     var aid = $(this).attr("href");
-    $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+    $('html,body').animate({scrollTop: $(aid).offset().top - 90},'slow');
 });
+
+window.onscroll = function() {
+    var header = document.getElementById('header');
+    $('#navbarNav').removeClass('show');
+    $('.navbar-toggler').addClass('collapsed');
+    if (window.pageYOffset > 90) {
+        header.style.cssText="background-color: black;";
+    }
+    else {
+        header.style.cssText="background-color: none;";
+    }
+};
